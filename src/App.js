5677a96f5d2bd7fc4d3205e9e73f577c37a87099
Albert Karagulov <albert.karagulov@mail.ru>
2023-12-25 23:32:38 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { MainLayout } from './layouts/MainLayout';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
+import { PasswordReset } from './pages/PasswordReset';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/password-reset" element={<PasswordReset />} />
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
       </Route>
