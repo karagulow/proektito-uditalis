d32b9d3cbc090493cfa137159cd52fb6590b001a
Albert Karagulov <albert.karagulov@mail.ru>
2023-12-25 22:01:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import './assets/scss/main.scss';
+import { MainLayout } from './layouts/MainLayout';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Menu } from '../components/Menu';
+
+export const MainLayout = () => {
+  return (
+    <div className="main">
+      <Menu />
+      <Outlet />
+    </div>
+  );
+};
